975491a3688575bd6b6eb4545caab81a12a8c43a
Anunay Argha <anunayargha@gmail.com>
2024-01-12 20:08:04 +0600

    navigation done

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@
 
 const navBtn = document.querySelector(".nav__menu");
 const headerEl = document.querySelector("header");
-const navContainer = document.querySelector(".nav_container");
+const navContainer = document.querySelector(".navigation_container");
 
 // ? FUNCTION -
 
@@ -10,11 +10,8 @@ const navObserver = new IntersectionObserver(
 	([e]) => {
 		if (!e.isIntersecting) {
 			navContainer.classList.add("sticky--nav");
-			navContainer.classList.remove('container')
 		} else {
 			navContainer.classList.remove("sticky--nav");
-			navContainer.classList.add('container')
-			
 		}
 	},
 	{
