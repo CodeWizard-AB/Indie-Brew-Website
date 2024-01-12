abf96872a710ecc203dbf93c63f4579db9f5f762
Anunay Argha <anunayargha@gmail.com>
2024-01-12 19:22:30 +0600

    project done

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,8 @@
 const navBtn = document.querySelector(".nav__menu");
 const navContainer = document.querySelector(".nav_container");
 
+// ? EVENT HANDLERS - 
+
 navBtn.addEventListener("click", function () {
 	navContainer.classList.toggle("nav--open");
 });
