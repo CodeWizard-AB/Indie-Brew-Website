15a9a44b5558eeac3000f3276fb3b724696386bd
Anunay Argha <anunayargha@gmail.com>
2024-01-12 20:01:24 +0600

    navigation bar on progress

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,28 @@
 
 const navBtn = document.querySelector(".nav__menu");
 const headerEl = document.querySelector("header");
+const navContainer = document.querySelector(".nav_container");
+
+// ? FUNCTION -
+
+const navObserver = new IntersectionObserver(
+	([e]) => {
+		if (!e.isIntersecting) {
+			navContainer.classList.add("sticky--nav");
+			navContainer.classList.remove('container')
+		} else {
+			navContainer.classList.remove("sticky--nav");
+			navContainer.classList.add('container')
+			
+		}
+	},
+	{
+		root: null,
+		threshold: 0.2,
+		rootMargin: `-${navContainer.getBoundingClientRect().height}px`,
+	}
+);
+navObserver.observe(headerEl);
 
 // ? EVENT HANDLERS -
 
